29d89b0d69ce17be937dc6209c9e7a28d61805ad
97gushan <97gustavh@gmail.com>
2020-11-18 14:01:18 +0100

    example program using the get and post request wrappers in the ArrowHead client lib

--- Program.cs
+++ Program.cs
@@ -1,14 +1,58 @@
 ﻿using System;
 using ArrowHead;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace arrowhead_client
 {
     class Program
     {
+        private ArrowHead.Http http;
+        private ArrowHead.Test t;
+        public Program()
+        {
+            this.http = new ArrowHead.Http("https://postman-echo.com/");
+            t = new ArrowHead.Test();
+
+        }
+
+        private void Run()
+        {
+            Console.WriteLine(this.t.GetHelloWorld());
+
+            this.PostExample();
+
+            while (true) ;
+        }
+
+        private async void PostExample()
+        {
+            Object json = new
+            {
+                a = "b",
+                c = "d",
+                e = new
+                {
+                    f = "g"
+                }
+            };
+            HttpResponseMessage resp = await this.http.Post("post", json);
+            string respMessage = await resp.Content.ReadAsStringAsync();
+            Console.WriteLine(JsonConvert.DeserializeObject(respMessage).data);
+        }
+
+        private async void GetExample()
+        {
+            HttpResponseMessage resp = await this.http.Get("get");
+            Console.WriteLine(resp);
+        }
+
         static void Main(string[] args)
         {
-            ArrowHead.Test t = new ArrowHead.Test();
-            Console.WriteLine(t.GetHelloWorld());
+            Program program = new Program();
+            program.Run();
         }
     }
 }
