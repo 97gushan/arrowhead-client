e8ead1b4820de32891d0b850b4449a0468fcb1cc
97gushan <97gustavh@gmail.com>
2020-11-30 16:36:33 +0100

    add working admin demo

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace Arrowhead_client
         private Arrowhead.Admin admin;
         private Arrowhead.Client client;
         private Arrowhead.Test t;
+
         public Program(string serviceName, string systemName, string certPath)
         {
             Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings();
@@ -19,30 +20,32 @@ namespace Arrowhead_client
             settings.SetServiceSettings(serviceName, new string[] { "HTTPS-SECURE-JSON" }, "/");
             settings.SetSystemSettings(systemName, "127.0.0.1", "8080");
             this.client = new Arrowhead.Client(settings);
-
         }
 
         public Program(string certPath)
         {
             Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings();
             settings.SetCertPath(certPath);
-            this.admin = new Arrowhead.Admin("19", "producer", "test_producer", settings);
+            settings.SetCloudSettings("testcloud2", "aitia");
+            settings.SetSystemSettings("test_producer", "127.0.0.1", "8080", "12");
+            settings.SetServiceSettings("producer3", new string[] { "HTTPS-SECURE-JSON" }, "");
+            this.admin = new Arrowhead.Admin(settings);
         }
 
         private void Run()
         {
-            Console.WriteLine(this.client.Orchestrate("producer"));
+            Console.WriteLine(this.client.Orchestrate("producer3"));
         }
 
         static void Main(string[] args)
         {
-            // Program producer = new Program("producer2", "test_producer", "/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/test_producer.p12");
+            // Program producer = new Program("producer3", "test_producer", "/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/test_producer.p12");
             // producer.Run();
 
-            Program consumer = new Program("consumer", "test_consumer", "/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/test_consumer.p12");
-            consumer.Run();
+            // Program consumer = new Program("consumer", "test_consumer", "/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/test_consumer.p12");
+            // consumer.Run();
 
-            // Program admin = new Program("/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/sysop.p12");
+            Program admin = new Program("/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/sysop.p12");
             // admin.Run();
         }
     }
