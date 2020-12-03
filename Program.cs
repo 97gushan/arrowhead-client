b8b2a758754ef26f7736eb183f39b2a0aedaa582
97gushan <97gustavh@gmail.com>
2020-12-03 15:07:53 +0100

    load consumer settings from json config file

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using Arrowhead;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace Arrowhead_client
@@ -11,7 +13,6 @@ namespace Arrowhead_client
     {
         private Arrowhead.Admin admin;
         private Arrowhead.Client client;
-        private Arrowhead.Test t;
 
         public Program(string serviceName, string systemName, string certPath)
         {
@@ -19,6 +20,12 @@ namespace Arrowhead_client
             settings.SetCertPath(certPath);
             settings.SetServiceSettings(serviceName, new string[] { "HTTPS-SECURE-JSON" }, "/");
             settings.SetSystemSettings(systemName, "127.0.0.1", "8080");
+            this.client = new Arrowhead.Client(settings);
+        }
+
+        public Program(JObject config)
+        {
+            Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings(config);
             this.client = new Arrowhead.Client(settings);
         }
 
@@ -30,12 +37,9 @@ namespace Arrowhead_client
             settings.SetSystemSettings("test_producer", "127.0.0.1", "8080", "12");
             settings.SetServiceSettings("producer3", new string[] { "HTTPS-SECURE-JSON" }, "");
             this.admin = new Arrowhead.Admin(settings);
+            this.admin.StoreOrchestrate();
         }
 
-        private void Run()
-        {
-            Console.WriteLine(this.client.Orchestrate("producer3"));
-        }
 
         static void Main(string[] args)
         {
@@ -45,7 +49,11 @@ namespace Arrowhead_client
             // Program consumer = new Program("consumer", "test_consumer", "/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/test_consumer.p12");
             // consumer.Run();
 
-            Program admin = new Program("/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/sysop.p12");
+            JObject config = JObject.Parse(File.ReadAllText(@"consumer_conf.json"));
+            // Console.WriteLine(config);
+            Program consumer = new Program(config);
+
+            // Program admin = new Program("/home/user/Projects/arrowhead/core-java-spring/certificates/testcloud2/sysop.p12");
             // admin.Run();
         }
     }
