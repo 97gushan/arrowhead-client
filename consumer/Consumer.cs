8c0d6e23302dd3fdc77e4de7d053489b859365a6
97gushan <97gustavh@gmail.com>
2020-12-07 14:41:36 +0100

    refactor into a separate producer and a consumer

---
+++ consumer/Consumer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Arrowhead;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace ArrowheadConsumer
+{
+    class Program
+    {
+        private Arrowhead.Admin admin;
+        private Arrowhead.Client client;
+
+        public Program(string serviceName, string systemName, string certPath)
+        {
+            Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings();
+            settings.SetCertPath(certPath);
+            settings.SetServiceSettings(serviceName, new string[] { "HTTPS-SECURE-JSON" }, "/");
+            settings.SetSystemSettings(systemName, "127.0.0.1", "8080");
+            this.client = new Arrowhead.Client(settings);
+        }
+
+        public Program(JObject consumerConfig, JObject adminConfig)
+        {
+            Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings(consumerConfig);
+            this.client = new Arrowhead.Client(settings);
+
+            Arrowhead.Utils.Settings adminSettings = new Arrowhead.Utils.Settings(adminConfig);
+            this.admin = new Arrowhead.Admin(adminSettings);
+            this.admin.StoreOrchestrate(this.client.GetSystemId());
+
+            Console.WriteLine(this.client.Orchestrate());
+        }
+
+        static void Main(string[] args)
+        {
+            JObject consumerConfig = JObject.Parse(File.ReadAllText(@"consumer.json"));
+            JObject adminConfig = JObject.Parse(File.ReadAllText(@"admin.json"));
+            Program consumer = new Program(consumerConfig, adminConfig);
+        }
+    }
+}
---
+++ producer/Producer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Arrowhead;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace ArrowheadProducer
+{
+    class Program
+    {
+        private Arrowhead.Client client;
+
+        public Program(JObject config)
+        {
+            Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings(config);
+            this.client = new Arrowhead.Client(settings);
+        }
+
+        static void Main(string[] args)
+        {
+            JObject config = JObject.Parse(File.ReadAllText(@"producer.json"));
+            Program producer = new Program(config);
+        }
+    }
+}
