4e10705d063e9f350bd3f23ba3271e890a6b0528
97gushan <97gustavh@gmail.com>
2020-12-08 15:01:22 +0100

    basic http server with a rest api

--- producer/Producer.cs
+++ producer/Producer.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.IO;
 using Arrowhead;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Grapevine.Shared;
+using Grapevine.Server;
+using Grapevine.Interfaces.Server;
+using Grapevine.Server.Attributes;
 
 
 namespace ArrowheadProducer
@@ -17,12 +18,38 @@ namespace ArrowheadProducer
         {
             Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings(config);
             this.client = new Arrowhead.Client(settings);
+
+            using (var server = new RestServer())
+            {
+                server.LogToConsole().Start();
+                Console.ReadLine();
+                server.Stop();
+            }
         }
 
         static void Main(string[] args)
         {
             JObject config = JObject.Parse(File.ReadAllText(@"producer.json"));
             Program producer = new Program(config);
+        }
+    }
+
+    [RestResource]
+    public sealed class TestResource
+    {
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/repeat")]
+        public IHttpContext RepeatMe(IHttpContext context)
+        {
+            var word = context.Request.QueryString["word"] ?? "what?";
+            context.Response.SendResponse(word);
+            return context;
+        }
+
+        [RestRoute]
+        public IHttpContext HelloWorld(IHttpContext context)
+        {
+            context.Response.SendResponse("Hello, world.");
+            return context;
         }
     }
 }
