a75ebd886a1923222b6af34e5133b23d332182a5
97gushan <97gustavh@gmail.com>
2020-12-09 11:33:41 +0100

    create basic consumer for demo

--- consumer/Consumer.cs
+++ consumer/Consumer.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
 using Arrowhead;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
+using Grapevine.Client;
+using Grapevine.Shared;
 
 
 namespace ArrowheadConsumer
@@ -14,14 +16,10 @@ namespace ArrowheadConsumer
         private Arrowhead.Admin admin;
         private Arrowhead.Client client;
 
-        public Program(string serviceName, string systemName, string certPath)
-        {
-            Arrowhead.Utils.Settings settings = new Arrowhead.Utils.Settings();
-            settings.SetCertPath(certPath);
-            settings.SetServiceSettings(serviceName, new string[] { "HTTPS-SECURE-JSON" }, "/");
-            settings.SetSystemSettings(systemName, "127.0.0.1", "8080");
-            this.client = new Arrowhead.Client(settings);
-        }
+        private string producerHost = "";
+        private string producerPort = "";
+        private string serviceUri = "";
+        private bool producerSSL = false;
 
         public Program(JObject consumerConfig, JObject adminConfig)
         {
@@ -32,7 +30,41 @@ namespace ArrowheadConsumer
             this.admin = new Arrowhead.Admin(adminSettings);
             this.admin.StoreOrchestrate(this.client.GetSystemId());
 
-            Console.WriteLine(this.client.Orchestrate());
+            JArray orchestrations = this.client.Orchestrate();
+            JObject orchestration = (JObject)orchestrations[0];
+
+            this.producerHost = orchestration.SelectToken("provider.address").ToString();
+            this.producerPort = orchestration.SelectToken("provider.port").ToString();
+            this.serviceUri = orchestration.SelectToken("serviceUri").ToString();
+            JArray interfaces = (JArray)orchestration.SelectToken("interfaces");
+
+            if (interfaces[0].SelectToken("interfaceName").ToString() == "HTTPs-SECURE-JSON")
+            {
+                producerSSL = true;
+            }
+            else
+            {
+                producerSSL = false;
+            }
+
+            Console.WriteLine("Orchestration against http" + (producerSSL ? "s://" : "://") + this.producerHost + ":" + this.producerPort + this.serviceUri + " was started");
+        }
+
+        public void ConsumeService()
+        {
+            RestClient client = new RestClient();
+
+            client.Host = this.producerHost;
+            client.Port = Int32.Parse(this.producerPort);
+
+            RestRequest req = new RestRequest(this.serviceUri + "/demo");
+            req.HttpMethod = HttpMethod.GET;
+            while (true)
+            {
+                RestResponse resp = (RestResponse)client.Execute(req);
+                Console.WriteLine(JsonConvert.DeserializeObject<JObject>(resp.GetContent()));
+                System.Threading.Thread.Sleep(1000);
+            }
         }
 
         static void Main(string[] args)
@@ -40,6 +72,7 @@ namespace ArrowheadConsumer
             JObject consumerConfig = JObject.Parse(File.ReadAllText(@"consumer.json"));
             JObject adminConfig = JObject.Parse(File.ReadAllText(@"admin.json"));
             Program consumer = new Program(consumerConfig, adminConfig);
+            consumer.ConsumeService();
         }
     }
 }
